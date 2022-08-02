7443d25cdd9dd40e0455f9cb9dd9a098fa8831e1
Bruno Henrique Lippert <brunolippert2002@gmail.com>
2022-08-02 19:51:31 -0300

    up

--- 2-border-radius_previewer/js/index.js
+++ 2-border-radius_previewer/js/index.js
@@ -1,3 +1,17 @@
 function modifyBorderRadius() {
+    let topLeft = document.getElementById("top-left").value
+    let topRigth = document.getElementById("top-rigth").value
+    let bottomLeft = document.getElementById("bottom-left").value
+    let bottomRigth = document.getElementById("bottom-rigth").value
 
+    if(topLeft == "" || topRigth == "" || bottomLeft == "" || bottomRigth == "") {
+        alert("Preencha todos os campos!") 
+    }
+
+    let modifiableBox = document.getElementById("modifiableBox").style.borderRadius = `${topLeft}% ${topRigth}% ${bottomLeft}% ${bottomRigth}%`
+
+    console.log(topLeft)
+    console.log(topRigth)
+    console.log(bottomLeft)
+    console.log(bottomRigth)
 }
