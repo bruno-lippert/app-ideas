19d6fba64e51895ed36fe1fa5b3e771af1af91ae
Bruno Henrique Lippert <brunolippert2002@gmail.com>
2022-08-05 14:00:46 -0300

    up

--- 2-border-radius_previewer/js/aplicacao.js
+++ 2-border-radius_previewer/js/aplicacao.js
@@ -5,62 +5,57 @@ class BorderRadius {
         this.bottomLeft = bottomLeft;
         this.bottomRigth = bottomRigth;
     }
+}
 
-    getInformations() {
-        let topLeft = document.getElementById("top-left").value;
-        let topRigth = document.getElementById("top-rigth").value;
-        let bottomLeft = document.getElementById("bottom-left").value;
-        let bottomRigth = document.getElementById("bottom-rigth").value;
-        let application = new BorderRadius(topLeft, topRigth, bottomLeft, bottomRigth);
-        // this.modifyBorderRadius(application)
-        console.log(application)
-        return application
-        
+function getInformations() {
+    let topLeft = document.getElementById("top-left").value;
+    let topRigth = document.getElementById("top-rigth").value;
+    let bottomLeft = document.getElementById("bottom-left").value;
+    let bottomRigth = document.getElementById("bottom-rigth").value;
+    let application = new BorderRadius(topLeft, topRigth, bottomLeft, bottomRigth);
+    // this.modifyBorderRadius(application)
+    console.log(application)
+    return application
+
+}
+
+function modifyBorderRadius() {
+    if (
+        application.topLeft == "" ||
+        application.topRigth == "" ||
+        application.bottomLeft == "" ||
+        application.bottomRigth == ""
+    ) {
+        alert("Preencha todos os campos!");
     }
 
-    modifyBorderRadius() {
-        if (
-            this.topLeft == "" ||
-            this.topRigth == "" ||
-            this.bottomLeft == "" ||
-            this.bottomRigth == ""
-        ) {
-            alert("Preencha todos os campos!");
-        }
+    document.getElementById("modifiableBox").style.borderRadius = `${application.topLeft}% ${application.topRigth}% ${application.bottomLeft}% ${application.bottomRigth}%`;
 
-        document.getElementById("modifiableBox").style.borderRadius = `${this.topLeft}% ${this.topRigth}% ${this.bottomLeft}% ${this.bottomRigth}%`;
+    console.log(application.topLeft);
+    console.log(application.topRigth);
+    console.log(application.bottomLeft);
+    console.log(application.bottomRigth);
+}
 
-        console.log(this.topLeft);
-        console.log(this.topRigth);
-        console.log(this.bottomLeft);
-        console.log(this.bottomRigth);
-    }
+// copyCSS(){
+//     this.addPropertiesBorderRadiusHTML(application)
+// }
 
-    // copyCSS(){
-    //     this.addPropertiesBorderRadiusHTML(application)
-    // }
-
-    copyCSS() {
-        let text = `border-top-left-radius:${this.topLeft};
+function copyCSS() {
+    let text = `border-top-left-radius:${this.topLeft};
                 border-top-right-radius:${this.topRigth};
                 border-bottom-left-radius:${this.bottomLeft};
                 border-bottom-right-radius:${this.bottomRigth};`;
-        console.log(text);
-        return text
+    console.log(text);
+    return text
 
-        // let element = document.getElementById("styleBorderRadius")
-        // element.innerText(text)
-    }
-
-    copyText() {
-        
-    }
-
-}    
+    // let element = document.getElementById("styleBorderRadius")
+    // element.innerText(text)
+}
 
 let application = new BorderRadius()
 
 function x() {
-    application.getInformations()
-    application.modifyBorderRadius()
+    getInformations()
+    modifyBorderRadius()
 }
