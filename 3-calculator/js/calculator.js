601619fc9ed3ea3cbcc4968af87fb1d91b8791ec
Bruno Henrique Lippert <brunolippert2002@gmail.com>
2022-08-06 15:50:14 -0300

    up

---
+++ 3-calculator/js/calculator.js
@@ -0,0 +1,29 @@
+class Calculator {
+    get value1(){
+        return 1
+    }
+    get value2(){
+        return 2
+    }
+    get value3(){
+        return 3
+    }
+    get value4(){
+        return 4
+    }
+    get value5(){
+        return 5
+    }
+    get value6(){
+        return 6
+    }
+    get value7(){
+        return 7
+    }
+    get value8(){
+        return 8
+    }
+    get value9(){
+        return 9
+    }
+}
