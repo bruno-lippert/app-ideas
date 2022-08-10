2a04afc58761293453b6f70336ff9adcdf50e949
Bruno Henrique Lippert <brunolippert2002@gmail.com>
2022-08-09 22:49:44 -0300

    up

--- 2-border-radius_previewer/js/aplicacao.js
+++ 2-border-radius_previewer/js/aplicacao.js
@@ -7,30 +7,45 @@ function getInformations() {
 }
 
 function modifyBorderRadius(topLeft, topRigth, bottomLeft, bottomRigth) {
-    if (
-        topLeft == "" ||
-        topRigth == "" ||
-        bottomLeft == "" ||
-        bottomRigth == ""
-    ) {
+    if (topLeft == "" || topRigth == "" || bottomLeft == "" || bottomRigth =="") {
         alert("Preencha todos os campos!");
+    } else {
+        //neste trecho é feito a modificação da do modifiable box
+        document.getElementById("modifiableBox").style.borderRadius = `${topLeft}% ${topRigth}% ${bottomLeft}% ${bottomRigth}%`;
+
+        //este trecho cria os textos que vão ser inseridos no HTML
+        let topLeftText =  document.createTextNode(`border-top-left-radius: ${topLeft};`);
+        let topRigthText =  document.createTextNode(`border-top-rigth-radius: ${topRigth};`);
+        let bottomLeftText =  document.createTextNode(`border-bottom-left-radius: ${bottomLeft};`);
+        let bottomRigthText =  document.createTextNode(`border-bottom-rigth-radius: ${bottomRigth};`);
+        
+        //neste trecho os textos estão sendo inseridos no HTML
+        let innerTopLeft = document.getElementById("topLeft")
+        innerTopLeft.appendChild(topLeftText)
+        let innerTopRigth = document.getElementById("topRigth")
+        innerTopRigth.appendChild(topRigthText)
+        let innerBottomLeft = document.getElementById("bottomLeft")
+        innerBottomLeft.appendChild(bottomLeftText)
+        let innerBottomRigth = document.getElementById("bottomRigth")
+        innerBottomRigth.appendChild(bottomRigthText)
     }
-
-    document.getElementById("modifiableBox").style.borderRadius = `${topLeft}% ${topRigth}% ${bottomLeft}% ${bottomRigth}%`;
-
-    let borderTopLeft = document.createTextNode(`border-top-left-radius:${topLeft};`);
-    let borderTopRigth = document.createTextNode(`border-top-right-radius:${topRigth};`)
-    let borderBottomLeft = document.createTextNode(`border-bottom-left-radius:${bottomLeft};`)
-    let borderBottomRigth = document.createTextNode(`border-bottom-right-radius:${bottomRigth};`)
-    let copiedText = document.getElementById("border-radius-properties")
 }
 
 function copyCSS() {
+    let topLeft = document.getElementById("topLeft").innerHTML
+    let topRigth = document.getElementById("topRigth").innerHTML
+    let bottomLeft = document.getElementById("bottomLeft").innerHTML
+    let bottomRigth = document.getElementById("bottomRigth").innerHTML
+    let text = topLeft + topRigth +  bottomLeft + bottomRigth
     
-    console.log(copiedText);
-    // copiedText.select();
-    // document.execCommand("copy")
-    return copiedText
+    navigator.clipboard.writeText(text)
+    .then(() => {
+    console.log("Text copied to clipboard...")
+})
+    .catch(err => {
+    console.log('Something went wrong', err);
+})
+    console.log(topLeft)
 }
 
 function modifyBox() {
