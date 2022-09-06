ec39b34d9a94bb8d1e7acbfc0d16039dcef0c074
Bruno Henrique Lippert <brunolippert2002@gmail.com>
2022-09-06 18:27:54 -0300

    create

--- 4-shopping-list/js/index.js
+++ 4-shopping-list/js/index.js
@@ -1,18 +1,35 @@
 function addItem() {
     const item = document.getElementById("item").value
-    
-    const newItemElement = document.createElement("p")
-    const newItem = document.createTextNode(item)
-    newItemElement.setAttribute("class", "items")
-    newItemElement.appendChild(newItem)
 
-    const container = document.getElementById("listOfItems")
-    container.appendChild(newItemElement)
-    const btnRemove = document.createElement("input")
-    btnRemove.setAttribute("type", "button")
-    btnRemove.setAttribute("value", "Remover")
-    btnRemove.setAttribute("onclick", "removeItem(this)")
-    newItemElement.appendChild(btnRemove)
+    if (item == "") {
+        alert("Digite um item para adiciona-lo na lista!")
+    } else {
+        //cira um paragrafo e adiciona o texto dentro dele
+        const newItemElement = document.createElement("p")
+        const newItem = document.createTextNode(item)
+        newItemElement.setAttribute("class", "items")
+        newItemElement.appendChild(newItem)
+
+        //cria um checkbox
+        const checkBox = document.createElement("input")
+        checkBox.setAttribute("type", "checkbox")
+
+        //coloca o checkbox e o item dentro da class container
+        const container = document.getElementById("listOfItems")
+        newItemElement.appendChild(checkBox)
+        const newDiv = document.createElement("div")
+        newDiv.setAttribute("class", "newDiv")
+        container.appendChild(newDiv)
+        newDiv.appendChild(newItemElement)
+
+        
+        //adiciona o botão de remover
+        const btnRemove = document.createElement("input")
+        btnRemove.setAttribute("type", "button")
+        btnRemove.setAttribute("value", "Remover")
+        btnRemove.setAttribute("onclick", "removeItem(this)")
+        newDiv.appendChild(btnRemove)
+    }
 }
 
 function removeItem(button) {
