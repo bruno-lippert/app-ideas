cfad0b17b310416bfef32ba778ef4c0f47e7882a
Bruno Henrique Lippert <brunolippert2002@gmail.com>
2022-09-17 13:10:43 -0300

    adição do botão 'remover tudo'

--- 4-shopping-list/js/index.js
+++ 4-shopping-list/js/index.js
@@ -1,48 +1,58 @@
 function addItem() {
-    const item = document.getElementById("item").value
+    const item = document.getElementById("item").value;
 
     if (item == "") {
-        alert("Digite um item para adiciona-lo na lista!")
-     } 
+        alert("Digite um item para adiciona-lo na lista!");
+    }
     //else if(item.length > 12){
-    // } 
+    // }
     else {
         //cira um paragrafo
-        const newItemElement = document.createElement("p")
-        const newItem = document.createTextNode(item)
-        newItemElement.setAttribute("class", "items")
-        
+        const newItemElement = document.createElement("p");
+        const newItem = document.createTextNode(item);
+        newItemElement.setAttribute("class", "items");
 
         //cria um checkbox
-        const checkBox = document.createElement("input")
-        checkBox.setAttribute("type", "checkbox")
-        checkBox.setAttribute("class", "checkBoxes")
+        const checkBox = document.createElement("input");
+        checkBox.setAttribute("type", "checkbox");
+        checkBox.setAttribute("class", "checkBoxes");
 
-        const span = document.createElement("span")
-        span.setAttribute("class", "checkmark")
-        
-        newItemElement.appendChild(checkBox)
-        newItemElement.appendChild(newItem)
+        const span = document.createElement("span");
+        span.setAttribute("class", "checkmark");
+
+        newItemElement.appendChild(checkBox);
+        newItemElement.appendChild(newItem);
 
         //coloca o checkbox e o item dentro da class container
-        const container = document.getElementById("listOfItems")
-        
-        const newDiv = document.createElement("div")
-        newDiv.setAttribute("class", "newDiv")
-        container.appendChild(newDiv)
-        newDiv.appendChild(newItemElement)
+        const container = document.getElementById("listOfItems");
 
-        
+        const newDiv = document.createElement("div");
+        newDiv.setAttribute("class", "newDiv");
+        container.appendChild(newDiv);
+        newDiv.appendChild(newItemElement);
+
         //adiciona o botão de remover
-        const btnRemove = document.createElement("input")
-        btnRemove.setAttribute("type", "button")
-        btnRemove.setAttribute("value", "Remover")
-        btnRemove.setAttribute("onclick", "removeItem(this)")
-        newDiv.appendChild(btnRemove)
+        const btnRemove = document.createElement("input");
+        btnRemove.setAttribute("type", "button");
+        btnRemove.setAttribute("value", "Remover");
+        btnRemove.setAttribute("onclick", "removeItem(this)");
+        newDiv.appendChild(btnRemove);
+
+        //habilita o botão removeAll
+        document.querySelector(".btnRemoveAll").disabled = false;
     }
 }
 
 function removeItem(button) {
-    const removableElement = button.parentNode
-    document.getElementById("listOfItems").removeChild(removableElement)
+    const removableElement = button.parentNode;
+    document.getElementById("listOfItems").removeChild(removableElement);
+}
+
+function removeAll() {
+    // const removableElement = button.parentNode
+    let b = document.getElementById("listOfItems");
+    b.innerHTML = ""
+    
+    //desabilita o botão removeAll
+    document.querySelector(".btnRemoveAll").disabled = true
 }
